edb8a29c720d9d7b66aad4d6bdbeeb924cf9dda7
Kevin Meinhardt <kmeinhardt7@gmail.com>
2020-12-28 15:03:25 +0000

    fix(app-two): fix mobile layout of controls

--- packages/app-two/components/TodoController.js
+++ packages/app-two/components/TodoController.js
@@ -4,14 +4,15 @@ const filters = ['all', 'active', 'complete'];
 
 export default function TodoController({ count, onSelect, onClear, value }) {
     return (
-        <Flex alignItems="center" justifyContent="space-between" p={4}>
-            <Box>
+        <Flex alignItems="center" justifyContent="space-between" p={4} flexWrap="wrap">
+            <Box m={2}>
                 {count} item{count > 1 ? 's' : ''} left
             </Box>
-            <Box>
+            <Box m={2}>
                 <HStack>
                     {filters.map((filter) => (
                         <Button
+                            size="sm"
                             variant={filter === value ? 'outline' : 'ghost'}
                             borderColor="red.200"
                             onClick={() => onSelect(filter)}
@@ -22,8 +23,8 @@ export default function TodoController({ count, onSelect, onClear, value }) {
                     ))}
                 </HStack>
             </Box>
-            <Box>
-                <Button onClick={onClear} variant="ghost">
+            <Box m={2}>
+                <Button onClick={onClear} variant="ghost" size="sm">
                     clear complete
                 </Button>
             </Box>
